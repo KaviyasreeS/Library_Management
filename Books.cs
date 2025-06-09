bc3fdb6136c688e507e99545b81244b3c794bf0c
KAVIYASREE S <u499116@wns.com>
2025-06-09 10:51:05 +0530

    First commit

---
+++ Books.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace libraryManagement;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+
+
+    public void displayDetails()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"ISBN: {ISBN}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using libraryManagement;
+Book b1 = new Book { Title = "Pride and prejustice", Author = "Jane Ausren", ISBN = "71762104024" };
+Book b2 = new Book { Title = "Drwaing on the Right Side of the brain", Author = "Kaviyasree", ISBN = "71762104031" };
+b1.displayDetails();
+b2.displayDetails();
